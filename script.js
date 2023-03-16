cb65fe5f1492b2bce21cb03ff35784a5c66ac583
Andrii <111511021+Poliuritank@users.noreply.github.com>
2023-03-17 01:03:51 +0200

    add js logic

--- script.js
+++ script.js
@@ -1,5 +1,66 @@
 const api = {
-	key: '28fd15358cdecbc1a1dfef367e71acef',
-	base: 'https://api.openweathermap.org/data/3.0/',
+	key: '9ff69c8cdd52144e87330ef32cec698c',
+	base: 'http://api.openweathermap.org/data/2.5/',
 };
+const searchINPUT = document.querySelector('.search');
+const submitBTN = document.querySelector('.btn');
+const error = document.querySelector('.error');
 
+// Display date
+const curDate = new Date();
+const date = document.querySelector('.date');
+let options = {
+	weekday: 'long',
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric',
+};
+const now = curDate.toLocaleString('en', options);
+date.innerText = now;
+//____________
+
+function getInput(e) {
+	e.preventDefault();
+	if (!searchINPUT.value) {
+		searchINPUT.placeholder = 'Введіть назву міста';
+
+		return;
+	}
+	if (e.type == 'click') {
+		getData(searchINPUT.value);
+	}
+}
+
+function getData() {
+	fetch(
+		`${api.base}weather?q=${searchINPUT.value}&units=metric&appid=${api.key}`
+	)
+		.then((response) => response.json())
+		.then(displayData);
+}
+
+function displayData(response) {
+	if (response.cod === '404') {
+		error.textContent = 'Enter the real name of the city';
+		searchINPUT.value = '';
+	} else {
+		console.log(response);
+		const city = document.querySelector('.city');
+		city.innerText = `${response.name}, ${response.sys.country}`;
+		error.textContent = '';
+		const temperature = document.querySelector('.temp__property');
+		temperature.innerHTML = `${response.main.temp} &deg;C`;
+		const weather = document.querySelector('.weather__property');
+		weather.innerText = `${response.weather[0].main}`;
+		const tempRange = document.querySelector('.temp-range__property');
+		tempRange.innerHTML = `${Math.floor(
+			response.main.temp_min
+		)}&deg;C - ${Math.round(response.main.temp_max)}&deg;C`;
+		const feels = document.querySelector('.feels__property');
+		feels.innerHTML = `${response.main.feels_like}&deg;C`;
+		const wind = document.querySelector('.wind__property');
+		wind.innerHTML = `${response.wind.speed} m/s`;
+	}
+}
+
+submitBTN.addEventListener('click', getInput);
